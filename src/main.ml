0f5c859669e2295ffbdf1697ca90cdfc7c01bf04
PedroAugusto2305 <p.augusto_23@outlook.com>
2023-12-17 10:37:10 -0300

    working with funcional

--- src/main.ml
+++ src/main.ml
@@ -1,12 +1,37 @@
- let hello () = 
-    print_endline "Hello, World!"
+(* definindo tipo de uma tarefa *)
+type status = Complete | Incomplete;; (* aqui definimos um tipo status que pode ser completo OU incompleto*)
+type task = { name: string; status: status };; (* aqui definimos o tipo task, que tem um name em string e status apontando pro tipoo anterior*)
 
 
+(* Adiciona uma tarefa à lista *)
+let add_task tasks name = 
+    { name = name; status = Incomplete } :: tasks;;
 
-let () = hello ()
+(* Listar todas as tarefas *)
+let rec list_tasks tasks =
+    match tasks with
+    | [] -> ()
+    | { name; status } :: rest ->
+        let status_str = match status with
+            | Complete -> "[Concluída]"
+            | Incomplete -> "[Pendente]"
+    in
+    print_endline (name ^ " " ^ status_str);
+    list_tasks rest;;
 
 
-let soma a b = a + b
+(* Marca uma tarefa como concluída *)
+let rec complete_task tasks name =
+    match tasks with
+    | [] -> []
+    | task :: rest -> 
+        if task.name = name then { task with status = Complete } :: rest
+        else task :: complete_task rest name;;
 
-let () = print_endline ("Soma: " ^ string_of_int (soma 10 4));
-
+(* Remove uma tarefa *)
+let rec remove_task tasks name = 
+    match tasks with
+    | [] -> []
+    | task :: rest ->
+        if task.name = name then rest
+        else task :: remove_task rest name;;
